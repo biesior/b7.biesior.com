871abf8ea62cfc4cb3873198be6d2672f65a506f
Marcus Biesioroff <biesior@gmail.com>
2022-09-04 18:08:42 +0200

    LocalExtensionRepository with instance package again...

---
+++ LocalExtensionRepository/package_b7_biesior_com/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('package_b7_biesior_com', 'Configuration/TypoScript', 'Package for B7 DeadEnd instance');
